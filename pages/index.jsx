d8d0c3b910852d53fef934b8f08c18354ee978c7
Riccardo Biffi <biffi.riccardo91@gmail.com>
2022-09-09 21:38:08 +0200

    Add pagination to fetchNFT and fetchNFTsForCollection

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,46 +4,45 @@ import { NFTCard } from '../components/nftCard';
 const Home = () => {
   const [wallet, setWalletAddress] = useState("");
   const [collection, setCollectionAddress] = useState("");
+  const [response, setResponce] = useState({});
   const [NFTs, setNFTs] = useState([]);
   const [fetchForCollection, setFetchForCollection] = useState(false);
-  const [page, setPagination] = useState(0);
-  const [nextToken, setNextToken] = useState();
-  const [previousToken, setPreviousToken] = useState();
+  const [pages, updatePages] = useState([0]);
+  const [currentPage, setCurrentPage] = useState(0);
 
   useEffect(
     () => {
       fetchForCollection ? fetchNFTsForCollection() : fetchNFTs();
     },
-    [page],
+    [currentPage],
   );
 
   const fetchNFTs = async () => {
     let nfts;
-    console.log("fetching nfts");
-    const api_key = "iE8Uo7aVv8BDUvhTI5fNIWueJxBvZGMk"
-    const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTs/`;
     var requestOptions = {
       method: 'GET'
     };
+    const api_key = "iE8Uo7aVv8BDUvhTI5fNIWueJxBvZGMk"
+    const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTs/`;
 
     if (!collection.length) {
-
-      const fetchURL = `${baseURL}?owner=${wallet}`;
+      const fetchURL = `${baseURL}?owner=${wallet}&pageKey=${pages[currentPage]}&pageSize=99`;
 
       nfts = await fetch(fetchURL, requestOptions).then(data => data.json())
+
     } else {
       console.log("fetching nfts for collection owned by address")
-      const fetchURL = `${baseURL}?owner=${wallet}&contractAddresses%5B%5D=${collection}&pageKey=${page}`;
+      const fetchURL = `${baseURL}?owner=${wallet}&contractAddresses%5B%5D=${collection}&pageKey=${pages[currentPage]}&pageSize=99`;
+
       nfts = await fetch(fetchURL, requestOptions).then(data => data.json())
     }
 
     if (nfts) {
-      console.log("nfts:", nfts)
+      console.log("Response:", nfts)
+      setResponce(nfts);
       setNFTs(nfts.ownedNfts)
     }
   }
-
-  //todo pagination for collections ${nextToken ? "&startToken=" + nextToken : ""}
 
   const fetchNFTsForCollection = async () => {
     if (collection.length) {
@@ -52,10 +51,13 @@ const Home = () => {
       };
       const api_key = "iE8Uo7aVv8BDUvhTI5fNIWueJxBvZGMk"
       const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTsForCollection/`;
-      const fetchURL = `${baseURL}?contractAddress=${collection}&withMetadata=${"true"}`;
+      const fetchURL = `${baseURL}?contractAddress=${collection}&withMetadata=${"true"}&startToken=${pages[currentPage]}&limit=99`;
+
       const nfts = await fetch(fetchURL, requestOptions).then(data => data.json());
+
       if (nfts) {
-        console.log("NFTs in collection:", nfts)
+        console.log("NFTs in collection:", nfts);
+        setResponce(nfts);
         setNFTs(nfts.nfts);
       }
     }
@@ -64,27 +66,34 @@ const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center py-8 gap-y-3">
       <div className="flex flex-col w-full justify-center items-center gap-y-2">
+
         <input
+          className='w-2/5 bg-slate-200 p-2 rounded-lg text-gray-800 text-center focus:outline-blue-300 disabled:bg-slate-50 disabled:text-gray-300'
           onChange={(e) => { setWalletAddress(e.target.value) }}
           value={wallet}
           type={"text"}
           disabled={fetchForCollection}
           placeholder="Add your wallet address" />
+
         <input
+          className='w-2/5 bg-slate-200 p-2 rounded-lg text-gray-800 text-center focus:outline-blue-300 disabled:bg-slate-50 disabled:text-gray-50'
           onChange={(e) => { setCollectionAddress(e.target.value) }}
           value={collection}
           type={"text"}
           placeholder="Add the collection address" />
+
         <label className="text-gray-600 ">
           <input type={"checkbox"}
             className="mr-2"
             onChange={(e) => { setFetchForCollection(e.target.checked) }}
           />
-          Fetch for collection
+          Fetch whole collection
         </label>
-        <button className={"disabled:bg-slate-500 text-white bg-blue-400 px-4 py-2 mt-3 rounded-sm w-1/5"}
+        <button
+          className={"disabled:bg-slate-500 text-white bg-blue-400 px-4 py-2 mt-3 rounded-sm w-1/5"}
           onClick={() => {
-            fetchForCollection ? fetchNFTsForCollection() : fetchNFTs();
+            updatePages([0]);
+            setCurrentPage(0); // hack activate fetching effect
           }}>
           Let's go!
         </button>
@@ -99,20 +108,38 @@ const Home = () => {
                 })
               }
             </div>
-            <div className='flex flex-wrap gap-y-12 mt-4 w-5/6 gap-x-2 justify-center'>
-              <button className={"m-auto disabled:bg-slate-500 text-white bg-blue-400 px-4 py-2 mt-3 rounded-sm w-1/5"}
+            <div className='flex flex-wrap gap-y-12 mt-10 w-5/6 gap-x-2 justify-center'>
+              <button
+                className={"m-auto disabled:bg-slate-500 text-white bg-blue-400 px-4 py-2 rounded-sm w-1/5"}
+                disabled={currentPage === 0}
                 onClick={() => {
-                  setPagination(page - 1);
-                  setNextToken(previousToken);
-                  setPreviousToken(previousToken); // todo find a way or rollback
+                  if (currentPage !== 0)
+                    setCurrentPage(currentPage - 1);
                 }}>
                 &lt;&lt; Previous page
               </button>
-              <button className={"m-auto disabled:bg-slate-500 text-white bg-blue-400 px-4 py-2 mt-3 rounded-sm w-1/5"}
+
+              <span className='text-center'>
+                {`[${1 + 99 * currentPage} - 
+                ${response.totalCount - 99 * currentPage < 99 ?
+                    response.totalCount :
+                    99 * (currentPage + 1)
+                  }]`}<br />
+                {`Of ${response.totalCount} NFTs`}
+              </span>
+
+              <button
+                className={"m-auto disabled:bg-slate-500 text-white bg-blue-400 px-4 py-2 rounded-sm w-1/5"}
+                disabled={response.totalCount - 99 * currentPage < 99}
                 onClick={() => {
-                  setPagination(page + 1);
-                  setPreviousToken(nextToken);
-                  setNextToken(nfts.nextToken);
+                  if (pages.length === currentPage + 1) {
+                    if (fetchForCollection)
+                      updatePages(pages => [...pages, response.nextToken]);
+                    else
+                      updatePages(pages => [...pages, response.pageKey]);
+                  }
+
+                  setCurrentPage(currentPage + 1);
                 }}>
                 Next page &gt;&gt;
               </button>
